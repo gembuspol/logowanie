afbeef2ee04c5a0fc7145e68680f1cb7e92622d1
Przemysław Gembalczyk <gembuspol@interia.pl>
2023-05-31 14:59:01 +0200

    dane
    Signed-off-by: Przemysław Gembalczyk <gembuspol@interia.pl>

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="pl">
 <head>
@@ -8,15 +11,52 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <?php
+        require_once "polaczenie.php";
+        if(isset($_POST['login'])){
+            $polaczenie=new mysqli($host,$uzytkownik,$haslo,$nazwaBazy);
+            if($polaczenie->connect_errno!=0){
+                echo "Błąd połączenia z bazą: ".$polaczenie->connect_errno." Opis błędu: ".$polaczenie->connect_error;
+            }else{
+                $sql="SELECT * FROM danelogowania";
+                $wynik=mysqli_query($polaczenie,$sql);
+                while($wiersz=mysqli_fetch_array($wynik) ){
+                    $imie[]=$wiersz['imie'];
+                    $nazwisko[]=$wiersz['nazwisko'];
+                }
+
+            }
+        }else{
+            echo "Podaj dane do logowania";
+        }
+        
+    ?>
+
     <header class="naglowek"><h1>Drużyna TOP Fortnite 4TIE</h1></header>
     <nav>
-        Logowanie
+        <form method="post" action="#">
+        <?php
+            if(isset($_SESSION['user'])){
+                echo "ustawiona sesja użytkownika";
+            }else{
+                echo "<label>Login:</label><input type='login' name='login'><label>Hasło:</label><input type='password' name='haslo'><input type='submit' value='Zaloguj się'>";
+            }
+        ?>
+        </form>
     </nav>
     <aside>
         <h2>Menu boczne</h2>
     </aside>
     <section class="prawy">
-        Treść strony
+        <output id="wynikLogowania">
+         <ul>
+            <?php
+            for($i=0;$i<count($imie);$i++){
+                echo "<li>$imie[$i] $nazwisko[$i]</li>";
+            }
+            ?>
+            </ul>
+        </output>
     </section>
     <footer>
         <h3>Stronę przygotował Przemek</h3>
