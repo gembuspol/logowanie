d82e6875415a51b5d229bb7023b9160ff9aff18d
Przemysław Gembalczyk <gembuspol@interia.pl>
2023-05-31 13:59:58 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Strona logowania</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header class="naglowek"><h1>Drużyna TOP Fortnite 4TIE</h1></header>
+    <nav>
+        Logowanie
+    </nav>
+    <aside>
+        <h2>Menu boczne</h2>
+    </aside>
+    <section class="prawy">
+        Treść strony
+    </section>
+    <footer>
+        <h3>Stronę przygotował Przemek</h3>
+    </footer>
+
+    
+</body>
+</html>
